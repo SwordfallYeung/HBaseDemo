f3d0512dee74e17f49457069ae79fecfb0816298
swordfall <2506597416@qq.com>
2019-01-25 02:34:15 +0800

    添加hbase on spark 的java和scala版本api

---
+++ v2120/src/main/java/cn/swordfall/hbaseOnSpark/HBaseOnBasicSpark.java
@@ -0,0 +1,10 @@
+package cn.swordfall.hbaseOnSpark;
+
+/**
+ * @Author: Yang JianQiu
+ * @Date: 2019/1/25 2:14
+ *  HBaseContent Usage Example
+ *  The example shows how HBaseContext can be used to do a foreachPartition on a RDD in Scala
+ */
+public class HBaseOnBasicSpark {
+}
---
+++ v2120/src/main/scala/cn/swordfall/hbaseOnSpark/HBaseOnBasicSpark.scala
@@ -0,0 +1,18 @@
+package cn.swordfall.hbaseOnSpark
+
+/**
+  * @Author: Yang JianQiu
+  * @Date: 2019/1/25 2:27
+  * HBaseContext Usage Example
+  * This example shows how HBaseContext can be used to do a foreachPartition on a RDD in Scala:
+  */
+class HBaseOnBasicSpark {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  def calculate(): Unit ={
+    val sc = new
+  }
+}
