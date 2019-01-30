753fede5fff7be867a356e2665cabf4d8a33e5e9
swordfall <2506597416@qq.com>
2019-01-31 02:03:52 +0800

    spark 读写hbase api demo

--- v2120/src/main/scala/cn/swordfall/hbaseOnSpark/HBaseOnBasicSpark.scala
+++ v2120/src/main/scala/cn/swordfall/hbaseOnSpark/HBaseOnBasicSpark.scala
@@ -1,20 +1,22 @@
 package cn.swordfall.hbaseOnSpark
 
-import org.apache.hadoop.hbase.client.{Put, Result}
+import org.apache.hadoop.hbase.TableName
+import org.apache.hadoop.hbase.client.{HBaseAdmin, Put, Result, TableDescriptorBuilder}
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
 import org.apache.hadoop.hbase.mapred.TableOutputFormat
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
 //import org.apache.hadoop.hbase.mapreduce.TableOutputFormat
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.hadoop.hbase.{HBaseConfiguration, HConstants}
 import org.apache.hadoop.mapred.JobConf
 import org.apache.hadoop.mapreduce.Job
-import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * @Author: Yang JianQiu
   * @Date: 2019/1/25 2:27
   * HBaseContext Usage Example
-  * This example shows how HBaseContext can be used to do a foreachPartition on a RDD in Scala:
+  * 参考资料：https://my.oschina.net/uchihamadara/blog/2032481
   */
 class HBaseOnBasicSpark {
 
@@ -22,56 +24,76 @@ class HBaseOnBasicSpark {
 
   }
 
-  /** spark 往hbase里面存放数据 **/
+  /** spark 往hbase里面写入数据 start **/
 
   /**
     * saveAsHadoopDataset
     */
   def writeToHBase(): Unit ={
+    // 屏蔽不必要的日志显示在终端上
+    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
 
-    val conf = new SparkConf().setAppName("SparkToHBase").setMaster("local")
-    val sc = new SparkContext(conf)
-    val input = sc.textFile("a.txt")
+    /* spark2.0以前的写法
+     val conf = new SparkConf().setAppName("SparkToHBase").setMaster("local")
+     val sc = new SparkContext(conf)
+    */
+    val sparkSession = SparkSession.builder().appName("SparkToHBase").master("local").getOrCreate()
+    val sc = sparkSession.sparkContext
+
+    val tableName = "test"
 
     //创建HBase配置
-    val hConf = HBaseConfiguration.create()
-    hConf.set(HConstants.ZOOKEEPER_QUORUM, "local:2181")
+    val hbaseConf = HBaseConfiguration.create()
+    hbaseConf.set(HConstants.ZOOKEEPER_QUORUM, "localhost") //设置zookeeper集群，也可以通过将hbase-site.xml导入classpath，但是建议在程序里这样设置
+    hbaseConf.set(HConstants.ZOOKEEPER_CLIENT_PORT, "2181") //设置zookeeper连接端口，默认2181
+    hbaseConf.set(TableOutputFormat.OUTPUT_TABLE, tableName)
 
-    //创建JobConf，设置输出格式和表名
-    val jobConf = new JobConf(hConf, this.getClass)
+    //初始化job，设置输出格式，TableOutputFormat 是 org.apache.hadoop.hbase.mapred 包下的
+    val jobConf = new JobConf(hbaseConf)
     jobConf.setOutputFormat(classOf[TableOutputFormat])
-    jobConf.set(TableOutputFormat.OUTPUT_TABLE, "test")
 
-    val data = input.map{ item =>
+    val dataRDD = sc.makeRDD(Array("2,jack,16", "1,Lucy,15", "5,mike,17", "3,Lily,14"))
+
+    val data = dataRDD.map{ item =>
       val Array(key, value) = item.split("\t")
       val rowKey = key.reverse
       val put = new Put(Bytes.toBytes(rowKey))
+      /*一个Put对象就是一行记录，在构造方法中指定主键
+      * 所有插入的数据 须用 org.apache.hadoop.hbase.util.Bytes.toBytes 转换
+      * Put.addColumn 方法接收三个参数：列族，列名，数据*/
       put.addColumn(Bytes.toBytes("f1"), Bytes.toBytes("info"), Bytes.toBytes(value))
       (new ImmutableBytesWritable(), put)
     }
     //保存到HBase表
     data.saveAsHadoopDataset(jobConf)
-    sc.stop()
+    sparkSession.stop()
   }
 
   /**
     * saveAsNewAPIHadoopDataset
     */
   def writeToHBaseNewAPI(): Unit ={
-    val conf = new SparkConf().setAppName("SparkToHBase").setMaster("local")
-    val sc = new SparkContext(conf)
-    val input = sc.textFile("a.txt")
+    // 屏蔽不必要的日志显示在终端上
+    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+    val sparkSession = SparkSession.builder().appName("SparkToHBase").master("local").getOrCreate()
+    val sc = sparkSession.sparkContext
 
-    val hConf = HBaseConfiguration.create()
-    hConf.set(HConstants.ZOOKEEPER_QUORUM, "localhost:2181")
+    val tableName = "test"
 
-    val jobConf = new JobConf(hConf, this.getClass)
-    jobConf.set(TableOutputFormat.OUTPUT_TABLE, "test")
+    val hbaseConf = HBaseConfiguration.create()
+    hbaseConf.set(HConstants.ZOOKEEPER_QUORUM, "localhost")
+    hbaseConf.set(HConstants.ZOOKEEPER_CLIENT_PORT, "2181")
+    hbaseConf.set(TableOutputFormat.OUTPUT_TABLE, tableName)
+
+    val jobConf = new JobConf(hbaseConf)
     //设置job的输出格式
     val job = Job.getInstance(jobConf)
     job.setOutputKeyClass(classOf[ImmutableBytesWritable])
     job.setOutputValueClass(classOf[Result])
     job.setOutputFormatClass(classOf[org.apache.hadoop.hbase.mapreduce.TableOutputFormat[ImmutableBytesWritable]])
+
+    val input = sc.textFile("a.txt")
+
     val data = input.map{item =>
       val Array(key, value) = item.split("\t")
       val rowKey = key.reverse
@@ -81,6 +103,32 @@ class HBaseOnBasicSpark {
     }
     //保存到HBase表
     data.saveAsNewAPIHadoopDataset(job.getConfiguration)
-    sc.stop()
+    sparkSession.stop()
+  }
+
+  /** spark 往hbase里面存放数据 end **/
+
+  /** spark 从hbase里面读取数据 start **/
+  def readFromHBaseNewAPI(): Unit ={
+    // 屏蔽不必要的日志显示在终端上
+    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+    val sparkSession = SparkSession.builder().appName("SparkToHBase").master("local").getOrCreate()
+    val sc = sparkSession.sparkContext
+
+    val tableName = "test"
+
+    val hbaseConf = HBaseConfiguration.create()
+    hbaseConf.set(HConstants.ZOOKEEPER_QUORUM, "localhost")
+    hbaseConf.set(HConstants.ZOOKEEPER_CLIENT_PORT, "2181") //设置zookeeper连接端口，默认2181
+    hbaseConf.set(TableOutputFormat.OUTPUT_TABLE, tableName)
+
+    //如果表不存在，则创建表
+    val admin = new HBaseAdmin(hbaseConf)
+    if(!admin.isTableAvailable(TableName.valueOf(tableName))){
+      val tdb = TableDescriptorBuilder.newBuilder(TableName.valueOf(tableName))
+      val td = tdb.build()
+      admin.createTable(td)
+    }
+    //保存到HBase表
   }
 }
