cb7873aa2b2a30e899f7e30228a24a91b73cd310
swordfall <2506597416@qq.com>
2019-02-12 14:01:29 +0800

    测试并修复bug

--- v2120/src/main/scala/cn/swordfall/hbaseOnSpark/HBaseOnBasicSpark.scala
+++ v2120/src/main/scala/cn/swordfall/hbaseOnSpark/HBaseOnBasicSpark.scala
@@ -23,10 +23,6 @@ import org.apache.hadoop.mapreduce.Job
   */
 class HBaseOnBasicSpark {
 
-  def main(args: Array[String]): Unit = {
-
-  }
-
   /** spark 往hbase里面写入数据 start **/
 
   /**
@@ -40,14 +36,14 @@ class HBaseOnBasicSpark {
      val conf = new SparkConf().setAppName("SparkToHBase").setMaster("local")
      val sc = new SparkContext(conf)
     */
-    val sparkSession = SparkSession.builder().appName("SparkToHBase").master("local").getOrCreate()
+    val sparkSession = SparkSession.builder().appName("SparkToHBase").master("local[4]").getOrCreate()
     val sc = sparkSession.sparkContext
 
     val tableName = "test"
 
     //创建HBase配置
     val hbaseConf = HBaseConfiguration.create()
-    hbaseConf.set(HConstants.ZOOKEEPER_QUORUM, "localhost") //设置zookeeper集群，也可以通过将hbase-site.xml导入classpath，但是建议在程序里这样设置
+    hbaseConf.set(HConstants.ZOOKEEPER_QUORUM, "192.168.187.201") //设置zookeeper集群，也可以通过将hbase-site.xml导入classpath，但是建议在程序里这样设置
     hbaseConf.set(HConstants.ZOOKEEPER_CLIENT_PORT, "2181") //设置zookeeper连接端口，默认2181
     hbaseConf.set(TableOutputFormat.OUTPUT_TABLE, tableName)
 
@@ -55,16 +51,17 @@ class HBaseOnBasicSpark {
     val jobConf = new JobConf(hbaseConf)
     jobConf.setOutputFormat(classOf[TableOutputFormat])
 
-    val dataRDD = sc.makeRDD(Array("2,jack,16", "1,Lucy,15", "5,mike,17", "3,Lily,14"))
+    val dataRDD = sc.makeRDD(Array("12,jack,16", "11,Lucy,15", "15,mike,17", "13,Lily,14"))
 
     val data = dataRDD.map{ item =>
-      val Array(key, value) = item.split(",")
+      val Array(key, name, age) = item.split(",")
       val rowKey = key.reverse
       val put = new Put(Bytes.toBytes(rowKey))
       /*一个Put对象就是一行记录，在构造方法中指定主键
       * 所有插入的数据 须用 org.apache.hadoop.hbase.util.Bytes.toBytes 转换
       * Put.addColumn 方法接收三个参数：列族，列名，数据*/
-      put.addColumn(Bytes.toBytes("f1"), Bytes.toBytes("info"), Bytes.toBytes(value))
+      put.addColumn(Bytes.toBytes("cf1"), Bytes.toBytes("name"), Bytes.toBytes(name))
+      put.addColumn(Bytes.toBytes("cf1"), Bytes.toBytes("age"), Bytes.toBytes(age))
       (new ImmutableBytesWritable(), put)
     }
     //保存到HBase表
@@ -78,13 +75,13 @@ class HBaseOnBasicSpark {
   def writeToHBaseNewAPI(): Unit ={
     // 屏蔽不必要的日志显示在终端上
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
-    val sparkSession = SparkSession.builder().appName("SparkToHBase").master("local").getOrCreate()
+    val sparkSession = SparkSession.builder().appName("SparkToHBase").master("local[4]").getOrCreate()
     val sc = sparkSession.sparkContext
 
     val tableName = "test"
 
     val hbaseConf = HBaseConfiguration.create()
-    hbaseConf.set(HConstants.ZOOKEEPER_QUORUM, "localhost")
+    hbaseConf.set(HConstants.ZOOKEEPER_QUORUM, "192.168.187.201")
     hbaseConf.set(HConstants.ZOOKEEPER_CLIENT_PORT, "2181")
     hbaseConf.set(org.apache.hadoop.hbase.mapreduce.TableOutputFormat.OUTPUT_TABLE, tableName)
 
@@ -95,13 +92,14 @@ class HBaseOnBasicSpark {
     job.setOutputValueClass(classOf[Result])
     job.setOutputFormatClass(classOf[org.apache.hadoop.hbase.mapreduce.TableOutputFormat[ImmutableBytesWritable]])
 
-    val input = sc.textFile("a.txt")
+    val input = sc.textFile("v2120/a.txt")
 
     val data = input.map{item =>
-      val Array(key, value) = item.split("\t")
+      val Array(key, name, age) = item.split(",")
       val rowKey = key.reverse
       val put = new Put(Bytes.toBytes(rowKey))
-      put.addColumn(Bytes.toBytes("f1"), Bytes.toBytes("info"), Bytes.toBytes(value))
+      put.addColumn(Bytes.toBytes("cf1"), Bytes.toBytes("name"), Bytes.toBytes(name))
+      put.addColumn(Bytes.toBytes("cf1"), Bytes.toBytes("age"), Bytes.toBytes(age))
       (new ImmutableBytesWritable, put)
     }
     //保存到HBase表
@@ -119,23 +117,15 @@ class HBaseOnBasicSpark {
   def readFromHBaseWithHBaseNewAPI(): Unit ={
     // 屏蔽不必要的日志显示在终端上
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
-    val sparkSession = SparkSession.builder().appName("SparkToHBase").master("local").getOrCreate()
+    val sparkSession = SparkSession.builder().appName("SparkToHBase").master("local[4]").getOrCreate()
     val sc = sparkSession.sparkContext
 
     val tableName = "test"
 
     val hbaseConf = HBaseConfiguration.create()
-    hbaseConf.set(HConstants.ZOOKEEPER_QUORUM, "localhost")
+    hbaseConf.set(HConstants.ZOOKEEPER_QUORUM, "192.168.187.201")
     hbaseConf.set(HConstants.ZOOKEEPER_CLIENT_PORT, "2181") //设置zookeeper连接端口，默认2181
     hbaseConf.set(org.apache.hadoop.hbase.mapreduce.TableInputFormat.INPUT_TABLE, tableName)
-
-    //如果表不存在，则创建表
-    val admin = new HBaseAdmin(hbaseConf)
-    if(!admin.isTableAvailable(TableName.valueOf(tableName))){
-      val tdb = TableDescriptorBuilder.newBuilder(TableName.valueOf(tableName))
-      val td = tdb.build()
-      admin.createTable(td)
-    }
 
     //读取数据并转化成rdd TableInputFormat是org.apache.hadoop.hbase.mapreduce包下的
     val hbaseRDD = sc.newAPIHadoopRDD(hbaseConf, classOf[org.apache.hadoop.hbase.mapreduce.TableInputFormat], classOf[ImmutableBytesWritable], classOf[Result])
@@ -148,7 +138,6 @@ class HBaseOnBasicSpark {
       val age = Bytes.toString(result.getValue("cf1".getBytes(), "age".getBytes()))
         println("Row key:" + key + "\tcf1.Name:" + name + "\tcf1.Age:" + age)
     }
-    admin.close()
 
     sparkSession.stop()
   }
@@ -165,12 +154,12 @@ class HBaseOnBasicSpark {
     val tableName = "test"
 
     val hbaseConf = HBaseConfiguration.create()
-    hbaseConf.set(HConstants.ZOOKEEPER_QUORUM, "localhost")
+    hbaseConf.set(HConstants.ZOOKEEPER_QUORUM, "192.168.187.201")
     hbaseConf.set(HConstants.ZOOKEEPER_CLIENT_PORT, "2181")
     hbaseConf.set(org.apache.hadoop.hbase.mapreduce.TableInputFormat.INPUT_TABLE, tableName)
 
     val scan = new Scan()
-    scan.addFamily(Bytes.toBytes("v"))
+    scan.addFamily(Bytes.toBytes("cf1"))
     val proto = ProtobufUtil.toScan(scan)
     val scanToString = new String(Base64.getEncoder.encode(proto.toByteArray))
     hbaseConf.set(org.apache.hadoop.hbase.mapreduce.TableInputFormat.SCAN, scanToString)
@@ -181,9 +170,9 @@ class HBaseOnBasicSpark {
     val dataRDD = hbaseRDD
       .map(x => x._2)
       .map{result =>
-        (result.getRow, result.getValue(Bytes.toBytes("v"), Bytes.toBytes("value")))
-      }.map(row => (new String(row._1), new String(row._2)))
+        (result.getRow, result.getValue(Bytes.toBytes("cf1"), Bytes.toBytes("name")), result.getValue(Bytes.toBytes("cf1"), Bytes.toBytes("age")))
+      }.map(row => (new String(row._1), new String(row._2), new String(row._3)))
       .collect()
-      .foreach(r => (println(r._1 + ":" + r._2)))
+      .foreach(r => (println("rowKey:"+r._1 + ", name:" + r._2 + ", age:" + r._3)))
   }
 }
