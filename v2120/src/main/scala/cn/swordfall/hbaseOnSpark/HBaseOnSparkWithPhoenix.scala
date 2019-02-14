febeef470878d39f6aff52cc00a8bb9b09df045b
swordfall <2506597416@qq.com>
2019-02-14 15:44:18 +0800

    测试spark通过phoenix读写hbase

--- v2120/src/main/scala/cn/swordfall/hbaseOnSpark/HBaseOnSparkWithPhoenix.scala
+++ v2120/src/main/scala/cn/swordfall/hbaseOnSpark/HBaseOnSparkWithPhoenix.scala
@@ -8,6 +8,9 @@ import org.apache.spark.sql.{SaveMode, SparkSession}
   * @Date: 2019/1/31 2:11
   * 参考资料：
   * https://www.cnblogs.com/wumingcong/p/6044038.html
+  * https://blog.csdn.net/zhuyu_deng/article/details/43192271
+  * https://www.jianshu.com/p/4c908e419b60
+  * https://blog.csdn.net/Colton_Null/article/details/83387995
   */
 class HBaseOnSparkWithPhoenix {
 
@@ -15,35 +18,42 @@ class HBaseOnSparkWithPhoenix {
     //屏蔽不必要的日志显示在终端上
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
 
-    val sparkSession = SparkSession.builder().appName("SparkHBaseDataFrame").master("local").getOrCreate()
+    val sparkSession = SparkSession.builder().appName("SparkHBaseDataFrame").master("local[4]").getOrCreate()
 
-    val url = s"jdbc:phoenix:192.168.187.201:2181"
-    val dbTable = "test"
+    //表小写，需要加双引号，否则报错
+    val dbTable = "\"test\""
 
     //spark 读取 phoenix 返回 DataFrame的第一种方式
     val rdf = sparkSession.read
       .format("jdbc")
       .option("driver", "org.apache.phoenix.jdbc.PhoenixDriver")
-      .option("url", url)
+      .option("url", "jdbc:phoenix:192.168.187.201:2181")
       .option("dbtable", dbTable)
       .load()
 
+    val rdfList = rdf.collect()
+    for (i <- rdfList){
+      println(i.getString(0) + " " + i.getString(1) + " " + i.getString(2))
+    }
     rdf.printSchema()
 
     //spark 读取 phoenix 返回 DataFrame的第二种方式
     val df = sparkSession.read
       .format("org.apache.phoenix.spark")
-      .options(Map("table" -> dbTable, "zkUrl" -> url))
+      .options(Map("table" -> dbTable, "zkUrl" -> "192.168.187.201:2181"))
       .load()
     df.printSchema()
-
-    //spark DataFrame 写入 phoenix，需要先建好表
-    /*df.write
-      .format("org.apache.phoenix.spark")
-      .mode(SaveMode.Overwrite)
-      .options(Map("table" -> "PHOENIXTESTCOPY", "zkUrl" -> url))
-      .save()
+    val dfList = df.collect()
+     for (i <- dfList){
+       println(i.getString(0) + " " + i.getString(1) + " " + i.getString(2))
+     }
+   //spark DataFrame 写入 phoenix，需要先建好表
+   /*df.write
+     .format("org.apache.phoenix.spark")
+     .mode(SaveMode.Overwrite)
+     .options(Map("table" -> "PHOENIXTESTCOPY", "zkUrl" -> "jdbc:phoenix:192.168.187.201:2181"))
+     .save()
 */
-    sparkSession.stop()
-  }
+   sparkSession.stop()
+ }
 }
