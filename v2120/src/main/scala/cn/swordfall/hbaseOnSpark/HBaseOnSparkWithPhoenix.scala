0a4ded5106671364c03286930510e873b14f986f
swordfall <2506597416@qq.com>
2019-02-15 11:12:31 +0800

    添加phoenix二级索引的测试样例

--- v2120/src/main/scala/cn/swordfall/hbaseOnSpark/HBaseOnSparkWithPhoenix.scala
+++ v2120/src/main/scala/cn/swordfall/hbaseOnSpark/HBaseOnSparkWithPhoenix.scala
@@ -1,7 +1,22 @@
 package cn.swordfall.hbaseOnSpark
 
+import java.net.URI
+import java.util.UUID
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.hbase.client.ConnectionFactory
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.hadoop.hbase.mapreduce.{HFileOutputFormat2, TableOutputFormat}
+import org.apache.hadoop.hbase.tool.LoadIncrementalHFiles
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.{HBaseConfiguration, HConstants, KeyValue, TableName}
+import org.apache.hadoop.mapreduce.Job
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkContext
 import org.apache.spark.sql.{SaveMode, SparkSession}
+
+import scala.collection.mutable.ListBuffer
 
 /**
   * @Author: Yang JianQiu
@@ -56,4 +71,71 @@ class HBaseOnSparkWithPhoenix {
 */
    sparkSession.stop()
  }
+
+  /**
+    * 测试demo ,使用搜狗实验室的数据http://www.sogou.com/labs/resource/q.php
+    * 批量插入 多列 测试phoenix的二级索引导入 1,724,264条数据的
+    * 二级索引覆盖索引
+    * https://www.cnblogs.com/MOBIN/p/5467284.html
+    * https://blog.csdn.net/sinat_36121406/article/details/82839003
+    * https://www.jianshu.com/p/a3c24638b498
+    */
+  def insertWithBulkLoadWithMultiTestDemo(): Unit ={
+
+    val sparkSession = SparkSession.builder().appName("insertWithBulkLoad").master("local[4]").getOrCreate()
+    val sc = sparkSession.sparkContext
+
+    val tableName = "log"
+    val hbaseConf = HBaseConfiguration.create()
+    hbaseConf.set(HConstants.ZOOKEEPER_QUORUM, "192.168.187.201")
+    hbaseConf.set(HConstants.ZOOKEEPER_CLIENT_PORT, "2181")
+    hbaseConf.set(TableOutputFormat.OUTPUT_TABLE, tableName)
+
+    val conn = ConnectionFactory.createConnection(hbaseConf)
+    val admin = conn.getAdmin
+    val table = conn.getTable(TableName.valueOf(tableName))
+
+    val job = Job.getInstance(hbaseConf)
+    //设置job的输出格式
+    job.setMapOutputKeyClass(classOf[ImmutableBytesWritable])
+    job.setMapOutputValueClass(classOf[KeyValue])
+    job.setOutputFormatClass(classOf[HFileOutputFormat2])
+    HFileOutputFormat2.configureIncrementalLoad(job, table, conn.getRegionLocator(TableName.valueOf(tableName)))
+
+    val rdd = sc.textFile("v2120/SogouQ.reduced")
+      .map(_.split("\t"))
+      .map(x => (UUID.randomUUID().toString, x(0), x(1), x(2), x(3), x(4)))
+      .sortBy(_._1)
+      .flatMap(x =>
+      {
+        val listBuffer = new ListBuffer[(ImmutableBytesWritable, KeyValue)]
+        val kv1: KeyValue = new KeyValue(Bytes.toBytes(x._1), Bytes.toBytes("cf1"), Bytes.toBytes("time"), Bytes.toBytes(x._2 + ""))
+        val kv2: KeyValue = new KeyValue(Bytes.toBytes(x._1), Bytes.toBytes("cf1"), Bytes.toBytes("userId"), Bytes.toBytes(x._3 + ""))
+        val kv3: KeyValue = new KeyValue(Bytes.toBytes(x._1), Bytes.toBytes("cf1"), Bytes.toBytes("keyWord"), Bytes.toBytes(x._4 + ""))
+        val kv4: KeyValue = new KeyValue(Bytes.toBytes(x._1), Bytes.toBytes("cf1"), Bytes.toBytes("ranking"), Bytes.toBytes(x._5 + ""))
+        val kv6: KeyValue = new KeyValue(Bytes.toBytes(x._1), Bytes.toBytes("cf1"), Bytes.toBytes("url"), Bytes.toBytes(x._6 + ""))
+        listBuffer.append((new ImmutableBytesWritable, kv3))
+        listBuffer.append((new ImmutableBytesWritable, kv4))
+        listBuffer.append((new ImmutableBytesWritable, kv1))
+        listBuffer.append((new ImmutableBytesWritable, kv6))
+        listBuffer.append((new ImmutableBytesWritable, kv2))
+        listBuffer
+      }
+      )
+    //多列的排序，要按照列名字母表大小来
+
+    isFileExist("hdfs://node1:9000/log", sc)
+
+    rdd.saveAsNewAPIHadoopFile("hdfs://node1:9000/log", classOf[ImmutableBytesWritable], classOf[KeyValue], classOf[HFileOutputFormat2], job.getConfiguration)
+    val bulkLoader = new LoadIncrementalHFiles(hbaseConf)
+    bulkLoader.doBulkLoad(new Path("hdfs://node1:9000/log"), admin, table, conn.getRegionLocator(TableName.valueOf(tableName)))
+  }
+
+  def isFileExist(filePath: String, sc: SparkContext): Unit ={
+    val output = new Path(filePath)
+    val hdfs = FileSystem.get(new URI(filePath), new Configuration)
+    if (hdfs.exists(output)){
+      hdfs.delete(output, true)
+    }
+  }
 }
