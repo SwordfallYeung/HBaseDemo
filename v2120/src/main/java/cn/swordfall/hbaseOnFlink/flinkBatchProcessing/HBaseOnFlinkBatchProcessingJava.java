55a798a53709e719e4dcf43fefef59739a1a1783
swordfall <2506597416@qq.com>
2019-03-15 00:48:15 +0800

    java版本，添加flink dataSet 批处理的读写hbase demo

--- v2120/src/main/java/cn/swordfall/hbaseOnFlink/flinkBatchProcessing/HBaseOnFlinkBatchProcessingJava.java
+++ v2120/src/main/java/cn/swordfall/hbaseOnFlink/flinkBatchProcessing/HBaseOnFlinkBatchProcessingJava.java
@@ -1,12 +1,45 @@
-package cn.swordfall.hbaseOnFlink.flinkBatchProcessing;/**
- * @Author: Yang JianQiu
- * @Date: 2019/3/15 0:41
- */
+package cn.swordfall.hbaseOnFlink.flinkBatchProcessing;
+
+import cn.swordfall.hbaseOnFlink.HBaseInputFormatJava;
+import cn.swordfall.hbaseOnFlink.HBaseOutputFormatJava;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
 
 /**
- * @author: Swordfall Yeung
- * @date:
- * @desc:
+ * @Author: Yang JianQiu
+ * @Date: 2019/3/15 0:41
+ *  flink dataSet 批处理读写HBase
+ *  读取HBase数据方式：实现TableInputFormat接口
+ *  写入HBase方式：实现OutputFormat接口
  */
 public class HBaseOnFlinkBatchProcessingJava {
+
+    /**
+     * 读取HBase数据方式：实现TableInputFormat接口
+     */
+    public void  readFromHBaseWithTableInputFormat(){
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        DataSet<Tuple2<String, String>> dataSet = env.createInput(new HBaseInputFormatJava());
+        dataSet.filter(new FilterFunction<Tuple2<String, String>>() {
+            @Override
+            public boolean filter(Tuple2<String, String> tuple2) throws Exception {
+                return tuple2.f0.startsWith("someStr");
+            }
+        });
+    }
+
+    /**
+     * 写入HBase方式：实现OutputFormat接口
+     */
+    public void write2HBaseWithOutputFormat(){
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        //2.定义数据
+        DataSet<String> dataSet = env.fromElements("zhangsan", "lisi", "wangwu", "zhaolilu");
+        dataSet.output(new HBaseOutputFormatJava());
+    }
 }
