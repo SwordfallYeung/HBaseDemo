9ce01eab6d0bfa83ea25304a4013b7ed7f6ad8b7
swordfall <2506597416@qq.com>
2019-03-15 00:18:08 +0800

    添加flink dataSet 批处理的读写hbase demo

--- v2120/src/main/scala/cn/swordfall/hbaseOnFlink/flinkBatchProcessing/HBaseOnFlinkBatchProcessing.scala
+++ v2120/src/main/scala/cn/swordfall/hbaseOnFlink/flinkBatchProcessing/HBaseOnFlinkBatchProcessing.scala
@@ -1,20 +1,37 @@
-package cn.swordfall.hbaseOnFlink
+package cn.swordfall.hbaseOnFlink.flinkBatchProcessing
 
+import cn.swordfall.hbaseOnFlink.{HBaseInputFormat, HBaseOutputFormat}
 import org.apache.flink.api.scala.{DataSet, ExecutionEnvironment}
 
 /**
   * @Author: Yang JianQiu
   * @Date: 2019/3/14 23:49
+  *
+  *  flink dataSet 批处理读写HBase
+  *  读取HBase数据方式：实现TableInputFormat接口
+  *  写入HBase方式：实现OutputFormat接口
   */
-class HBaseBatch {
+class HBaseOnFlinkBatchProcessing {
 
-  def main(args: Array[String]): Unit = {
+  /**
+    * 读取HBase数据方式：实现TableInputFormat接口
+    */
+  def readFromHBaseWithTableInputFormat(): Unit ={
     val env = ExecutionEnvironment.getExecutionEnvironment
-    env.createInput(new HBaseInputFormat)
-    
-    //2.定义数据 stu(age, name, height)
+
+    val dataStream = env.createInput(new HBaseInputFormat)
+    dataStream.filter(x => x._1.startsWith("someStr")).print()
+  }
+
+  /**
+    * 写入HBase
+    * 第二种：实现OutputFormat接口
+    */
+  def write2HBaseWithOutputFormat(): Unit = {
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+
+    //2.定义数据
     val dataSet: DataSet[String] = env.fromElements("zhangsan", "lisi", "wangwu", "zhaolilu")
     dataSet.output(new HBaseOutputFormat)
-
   }
 }
